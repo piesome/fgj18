dfe9c73b9b523b847c2ae0adad53e4a184f3f976
Minttu Imberg <minttu@rakettitiede.com>
2018-01-26 20:54:20 +0200

    movable ship

--- ship.lua
+++ ship.lua
@@ -14,7 +14,19 @@ function Ship:draw()
 end
 
 function Ship:update(dt)
-    velocityVector = cpml.vec2.new(1, 0)
+    velocityVector = cpml.vec2.new(0, 0)
+    if love.keyboard.isDown("left") then
+        velocityVector = velocityVector - cpml.vec2.new(1, 0)
+    end
+    if love.keyboard.isDown("right") then
+        velocityVector = velocityVector - cpml.vec2.new(-1, 0)
+    end
+    if love.keyboard.isDown("up") then
+        velocityVector = velocityVector - cpml.vec2.new(0, 1)
+    end
+    if love.keyboard.isDown("down") then
+        velocityVector = velocityVector - cpml.vec2.new(0, -1)
+    end
     self.position = self.position + velocityVector
 end
 
