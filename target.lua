3635978a5d53a542013faede7d756ca6e7708266
Minttu Imberg <minttu@rakettitiede.com>
2018-01-27 10:42:50 +0200

    target image

--- target.lua
+++ target.lua
@@ -1,5 +1,7 @@
 Class = require "hump.class"
 
+local targetImage = love.graphics.newImage("assets/graphics/waypoint.png")
+
 Target = Class{}
 
 function Target:init(number, position)
@@ -8,6 +10,8 @@ function Target:init(number, position)
 end
 
 function Target:draw()
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.draw(targetImage, self.position.x, self.position.y, 0, 1, 1, targetImage:getWidth() / 2, targetImage:getHeight() / 2)
     love.graphics.print(tostring(self.number), self.position.x, self.position.y)
 end
 
