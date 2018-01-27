ace7287bd6633246793dadda4b25b6ea305a374d
Nicklas Ahlskog <kivi@kivibot.fi>
2018-01-27 15:58:08 +0200

    EXPLOSION

--- particleEffects.lua
+++ particleEffects.lua
@@ -7,7 +7,7 @@ ParticleEmitter = require "particleEmitter"
 effects = {}
 
 function effects:explosion(particles, position, velocity)
-    local emitter = ParticleEmitter(position, velocity / 3, 5, {255, 127, 0, 64}, 1, 1, 0.3, 100)
+    local emitter = ParticleEmitter(position, velocity / 2, 5, {255, 127, 0, 64}, 0.5, 1.7, 0.6, 20)
     emitter:update(1, particles)
 end
 
