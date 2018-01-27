470bbfd8656a22329f1b3772ed2788c76c4dbb13
Minttu Imberg <minttu@rakettitiede.com>
2018-01-27 10:19:42 +0200

    use ship gfx, basic ship movement

--- ship.lua
+++ ship.lua
@@ -1,6 +1,8 @@
 Class = require "hump.class"
 cpml = require "cpml"
 
+local shipImage = love.graphics.newImage("assets/graphics/playership.png")
+
 -- Converts HSL to RGB. (input and output range: 0 - 255)
 function HSL(h, s, l, a)
 	if s<=0 then return l,l,l,a end
@@ -19,6 +21,8 @@ end
 
 Ship = Class
     { position = cpml.vec2.new(100, 100)
+    , velocity = cpml.vec2.new(0, 0)
+    , rotation = 0
     , heatGeneration = 0      -- Current heat increase per second
     --, heatStored = 0        -- heat stored inside the ship
     , surfaceHeat = 100       -- heat stored on the surface
@@ -39,15 +43,13 @@ function Ship:heatCircle(heatPercentage, radius)
 end
 
 function Ship:draw()
-
     for x=1,50 do
         r = 15 + x*x/4
         self:heatCircle(self:radiationAtDistance(r)*2, r)
     end
-    love.graphics.setColor(HSL(0, 255, math.min(self.surfaceHeat*0.1, 255)))
-    love.graphics.print("@", self.position.x-17, self.position.y-20) -- XXX: manual centering
 
-    love.graphics.setColor(100,100,100)
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.draw(shipImage, self.position.x, self.position.y, self.rotation, 1, 1, shipImage:getWidth() / 2, shipImage:getHeight() / 2)
 end
 
 function Ship:radiationAtDistance(d)
@@ -68,24 +70,31 @@ function Ship:update(dt)
     self.heatGeneration = 0
     velocityVector = cpml.vec2.new(0, 0)
 
-    function move(x, y)
-        velocityVector = velocityVector - cpml.vec2.new(x, y)
+    function move(thrust)
+        velocityVector = velocityVector - cpml.vec2.new(0, thrust)
         self.heatGeneration = self.heatGeneration + self.thrusterHeat * dt
     end
 
+    function rotate(rad)
+        self.rotation = self.rotation + rad
+    end
+
     if love.keyboard.isDown("left") then
-        move(1,0)
+        rotate(-1 * dt)
     end
     if love.keyboard.isDown("right") then
-        move(-1,0)
+        rotate(1 * dt)
     end
     if love.keyboard.isDown("up") then
-        move(0,1)
+        move(1 * dt)
     end
     if love.keyboard.isDown("down") then
-        move(0,-1)
+        move(-1 * dt)
     end
-    self.position = self.position + velocityVector
+    self.velocity = self.velocity + cpml.vec2.rotate(cpml.vec2.normalize(velocityVector), self.rotation)
+    self.position = self.position + self.velocity * dt
+    self.velocity = self.velocity - (self.velocity * dt * 0.5)
+
     self:heatUpdate(dt)
 end
 
