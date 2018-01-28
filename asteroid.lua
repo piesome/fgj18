6c91daada61caf6a0daf7836b837d72c0cf1f38a
Minttu Imberg <minttu@rakettitiede.com>
2018-01-28 03:23:46 +0200

    asteroid texture generator

--- asteroid.lua
+++ asteroid.lua
@@ -1,6 +1,8 @@
 Class = require "hump.class"
 cpml = require "cpml"
 vec2 = cpml.vec2
+
+require "util"
 require "rayCasting"
 
 Asteroid = Class{
@@ -11,6 +13,17 @@ Asteroid = Class{
         self.segmentCount = math.min(13, (radius / 3))
         self.points = {}
         self.pointVectors = {}
+        self.edges = {9999999, 9999999, -9999999, -9999999}
+        function emax(i, p)
+            if self.edges[i] < p then
+                self.edges[i] = p
+            end
+        end
+        function emin(i, p)
+            if self.edges[i] > p then
+                self.edges[i] = p
+            end
+        end
         each = ((math.pi * 2) / self.segmentCount)
         for i=1, self.segmentCount do
             rad = (each * i)
@@ -19,6 +32,11 @@ Asteroid = Class{
             table.insert(self.points, point.x)
             table.insert(self.points, point.y)
             table.insert(self.pointVectors, point)
+
+            emin(1, point.x)
+            emin(2, point.y)
+            emax(3, point.x)
+            emax(4, point.y)
         end
         self.convexHullVectors = convexHull(self.pointVectors)
         self.convexHull = {}
@@ -26,14 +44,45 @@ Asteroid = Class{
             table.insert(self.convexHull, vec.x)
             table.insert(self.convexHull, vec.y)
         end
+
+        local twidth = self.edges[3] - self.edges[1]
+        local theight = self.edges[4] - self.edges[2]
+
+        local canvas = love.graphics.newCanvas(twidth, theight)
+        love.graphics.setCanvas(canvas)
+
+        love.graphics.push()
+        love.graphics.translate(-self.edges[1], -self.edges[2])
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.polygon("fill", self.points)
+        love.graphics.pop()
+
+        love.graphics.setCanvas()
+        local xx = self.edges[1]
+        local yy = self.edges[2]
+
+        function gen(x, y, r, g, b, a)
+            if r + g + b == 0 then
+                return 0, 0, 0, 0
+            end
+
+            local value = ((love.math.noise(xx + x / 32, yy + y / 32) * (love.math.noise(xx + x / 18, yy + y / 18)) * 2) - 1) * 0.15
+            return HSL(32 / 360 * 255, 0.15 * 255, math.floor(((0.3 + value) * 16) + 0.5) * 16, 255)
+        end
+
+        local id = canvas:newImageData()
+        id:mapPixel(gen)
+
+        self.image = love.graphics.newImage(id)
     end,
     draw = function(self)
-        love.graphics.setColor(self.color)
-        love.graphics.polygon("fill", self.points)
+        love.graphics.draw(self.image, self.edges[1], self.edges[2])
+        --love.graphics.setColor(self.color)
+        --love.graphics.polygon("fill", self.points)
 
         -- debug hull
-        love.graphics.setColor(255,0,255)
-        love.graphics.polygon("line", self.convexHull)
+        --love.graphics.setColor(255,0,255)
+        --love.graphics.polygon("line", self.convexHull)
     end,
 }
 
--- util.lua
+++ util.lua
@@ -28,3 +28,19 @@ function drawinrect(img, x, y, w, h, r, ox, oy, kx, ky)
     return -- tail call for a little extra bit of efficiency
     love.graphics.draw(img, x, y, r, w / img:getWidth(), h / img:getHeight(), ox, oy, kx, ky)
 end
+
+-- Converts HSL to RGB. (input and output range: 0 - 255)
+function HSL(h, s, l, a)
+	if s<=0 then return l,l,l,a end
+	h, s, l = h/256*6, s/255, l/255
+	local c = (1-math.abs(2*l-1))*s
+	local x = (1-math.abs(h%2-1))*c
+	local m,r,g,b = (l-.5*c), 0,0,0
+	if     h < 1 then r,g,b = c,x,0
+	elseif h < 2 then r,g,b = x,c,0
+	elseif h < 3 then r,g,b = 0,c,x
+	elseif h < 4 then r,g,b = 0,x,c
+	elseif h < 5 then r,g,b = x,0,c
+	else              r,g,b = c,0,x
+	end return (r+m)*255,(g+m)*255,(b+m)*255,a
+end
