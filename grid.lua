955fd383bedf37b4fef6f4f43f0ec61b610ffe07
Minttu Imberg <minttu@rakettitiede.com>
2018-01-28 03:56:24 +0200

    larger & dimmer grid

--- grid.lua
+++ grid.lua
@@ -2,8 +2,8 @@ Class = require "hump.class"
 
 Grid = Class{
     init = function(self, width, height)
-        self.spacing = 50
-        self.color = {255, 255, 255, 100}
+        self.spacing = 250
+        self.color = {255, 255, 255, 50}
         self.width = width
         self.height = height
     end,
