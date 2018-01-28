2ce5e786eeec405bc7bb7f43f29c8a0563c56271
Tuomas Kinnunen <tuomas.kinnunen@aalto.fi>
2018-01-28 12:44:04 +0200

    remove debug lines

--- enemy.lua
+++ enemy.lua
@@ -25,9 +25,6 @@ end
 function Enemy:draw()
     love.graphics.setColor(255, 255, 255)
     love.graphics.draw(enemyImage, self.position.x, self.position.y, self.rotation, 1, 1, enemyImage:getWidth() / 2, enemyImage:getHeight() / 2)
-    if self.test ~= nil then
-        love.graphics.line(self.position.x, self.position.y, self.test[2].x, self.test[2].y)
-    end
 end
 
 function cpml.vec2.angle_between2(a, b)
@@ -45,12 +42,6 @@ function Enemy:update(dt, ship, particles, projectiles, asteroidField)
     --print(playerHeatDetected)
     local acceleration = cpml.vec2.new(0, 0)
     orientationVec = cpml.vec2(0,-1):rotate(self.rotation)
-    local test = asteroidField:testRay(self.position, toPlayerVec, true)
-    if test ~= nil then
-        self.test = test
-    else
-        self.test = nil
-    end
     if playerHeatDetected > self.sensorSensitivity and
             asteroidField:testRay(self.position, toPlayerVec, true) == nil then -- testRay is slower than radiation test
 
